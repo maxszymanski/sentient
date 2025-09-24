78fac94cb24d8e12b9328889478a92314ec7736d
Maksymilian Szymański <maksymilianszymanski2@gmail.com>
2025-09-24 17:26:00 +0200

    fix index on section on mobile

--- src/app/_components/_ui/LargeEllipse.tsx
+++ src/app/_components/_ui/LargeEllipse.tsx
@@ -3,7 +3,7 @@ import LargeEllipseBg from '@/assets/large-ellipse.webp'
 function LargeEllipse() {
     return (
         <div
-            className="top-30 -right-50 absolute z-50 h-full max-h-[1570px] w-[956px] max-w-full bg-contain bg-no-repeat md:z-[60]"
+            className="top-30 -right-50 absolute z-[48] h-full max-h-[1570px] w-[956px] max-w-full bg-contain bg-no-repeat md:z-[60]"
             style={{
                 backgroundImage: `url(${LargeEllipseBg.src})`,
             }}
